f9b32d8575907737ce45e3abbdabbab9ca11e442
M Arbo <marbo.phd@gmail.com>
2022-12-31 14:32:11 -0600

    Add Page Seeder

---
+++ app/database/seeders/PageSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Page::create([
+            'name' => "Index",
+            'slug' => "index",
+            'content' => "Lorem Ipsum"
+        ]);
+    }
+}
